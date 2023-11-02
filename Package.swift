2d7f3e7864585b31adbcc66ef378a99fd996378d
Valerii Hvozdiev <valerii@futurae.com>
2023-11-02 15:34:58 +0200

    Added version v2.3.6

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "FuturaeKit",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "FuturaeKit", targets: ["FuturaeKit"])
+    ],
+    targets: [
+        .binaryTarget(
+          name: "FuturaeKit",
+          url: "in_progress",
+          checksum: "in_progress"
+        )
+    ]
+)
