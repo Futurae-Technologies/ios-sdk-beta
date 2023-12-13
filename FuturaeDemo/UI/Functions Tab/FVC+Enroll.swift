11d30cb335d34b4eeec80adbeb9c9764aa845a94
Valerii Hvozdiev <valerii@futurae.com>
2023-12-13 14:31:31 +0200

    Update framework to version 3.0.0-beta

---
+++ FuturaeDemo/UI/Functions Tab/FVC+Enroll.swift
@@ -0,0 +1,104 @@
+//
+//  FunctionsViewController+Enroll.swift
+//  FuturaeDemo
+//
+//  Created by Armend Hasani on 8.12.23.
+//  Copyright © 2023 Futurae. All rights reserved.
+//
+
+import Foundation
+import FuturaeKit
+
+extension FunctionsViewController {
+    @IBAction func enrollTouchedUpInside(_ sender: UIButton) {
+        enrollWithPin = false
+        presentQRCodeControllerWithQRCodeType(.enrollment, sender: sender)
+    }
+
+    @IBAction func enrollWithPinTouchedUpInside(_ sender: UIButton) {
+        enrollWithPin = true
+        presentQRCodeControllerWithQRCodeType(.enrollment, sender: sender)
+    }
+
+    @IBAction func enrollShortCodeTouchedUpInside(_ sender: UIButton) {
+        enrollShortCodeWithPin(false)
+    }
+
+    @IBAction func enrollShortCodeWithPinTouchedUpInside(_ sender: UIButton) {
+        enrollShortCodeWithPin(true)
+    }
+    
+    func enrollWithQRCode(_ QRCodeResult: String) {
+        if enrollWithPin {
+            guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "pinViewController") as? PinViewController else { return }
+            vc.pinMode = .set
+            vc.didFinishWithPin = { [weak self] pin in
+                FTRClient.shared.enroll(EnrollParameters.with(activationCode: QRCodeResult, sdkPin: pin ?? ""), success: {
+                    self?.dismiss(animated: true) {
+                        self?.showAlert(title: "Success", message: "User account enrolled successfully!")
+                        self?.loadServiceLogo()
+                    }
+                }, failure: { error in
+                    self?.showAlert(title: "Error", message: error.localizedDescription)
+                })
+            }
+
+            dismiss(animated: true) {
+                self.present(vc, animated: true, completion: nil)
+            }
+        } else {
+            FTRClient.shared.enroll(EnrollParameters.with(activationCode: QRCodeResult), success: { [weak self] in
+                self?.dismiss(animated: true) {
+                    self?.showAlert(title: "Success", message: "User account enrolled successfully!")
+                    self?.loadServiceLogo()
+                }
+            }, failure: { [weak self] error in
+                self?.dismiss(animated: true) {
+                    self?.showAlert(title: "Error", message: error.localizedDescription)
+                }
+            })
+        }
+    }
+
+    func enrollShortCodeWithPin(_ withPin: Bool) {
+        guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "pinViewController") as? PinViewController else { return }
+        vc.pinLength = 19
+        vc.secureText = false
+        vc.pinMode = .shortCode
+        vc.didFinishWithPin = { [weak self] pin in
+            self?.dismiss(animated: true) {
+                self?.enrollWithShortCode(pin ?? "", enrollWithPin: withPin)
+            }
+        }
+
+        present(vc, animated: true, completion: nil)
+    }
+
+    func enrollWithShortCode(_ code: String, enrollWithPin withPin: Bool) {
+        if withPin {
+            guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "pinViewController") as? PinViewController else { return }
+            vc.pinMode = .set
+            vc.didFinishWithPin = { [weak self] pin in
+                FTRClient.shared.enroll(EnrollParameters.with(shortCode: code, sdkPin: pin ?? ""), success: {
+                    self?.dismiss(animated: true) {
+                        self?.showAlert(title: "Success", message: "User account enrolled successfully!")
+                        self?.loadServiceLogo()
+                    }
+                }, failure: { error in
+                    self?.showAlert(title: "Error", message: error.localizedDescription)
+                })
+            }
+
+            present(vc, animated: true, completion: nil)
+        } else {
+            FTRClient.shared.enroll(EnrollParameters.with(shortCode:code), success: {
+                self.dismiss(animated: true) {
+                    self.showAlert(title: "Success", message: "User account enrolled successfully!")
+                    self.loadServiceLogo()
+                }
+            }, failure: { error in
+                self.showAlert(title: "Error", message: error.localizedDescription)
+            })
+        }
+    }
+}
