c9b8c0c131528a801346c8355294dd02433c0951
Valerii Hvozdiev <valerii@futurae.com>
2024-02-12 13:02:53 +0200

    Update framework to version 3.1.1-beta

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FuturaeKit",
-            url: "https://github.com/Futurae-Technologies/ios-sdk-beta/releases/download/v3.1.0-beta/FuturaeKit-v3.1.0.xcframework.zip",
-            checksum: "874301c2635eab05abdad1517b5bd703a9e180a78e961daef1aae655ef5726cc"
+            url: "https://github.com/Futurae-Technologies/ios-sdk-beta/releases/download/v3.1.1-beta/FuturaeKit-v3.1.1.xcframework.zip",
+            checksum: "0819bb349895105c941d2a9b77ab4ed07ddf3c58dd918d7bbe8031fde2f57b72"
         ),
         .target(
             name: "FuturaeKitUmbrella",
